5dd9a78be8a0aa01dbed0475df2897befbc32c33
olara3 <felipe.pinga@gmail.com>
2020-05-26 18:38:09 -0300

    Add files via upload
    comite de hj 26/05

--- funcoes.js
+++ funcoes.js
@@ -1,37 +1,40 @@
 function autenticar(){
-    var txtEmail  = document.getElementById("txtEmail").value;
+    // passo 1 capturo informações do formulário
+    var txtEmail = document.getElementById("txtEmail").value;
     var txtSenha = document.getElementById("txtSenha").value;
-    console.log(txtEmail+ ' / ' + txtSenha);
-    // MONTEI O corpo da mensagem
+
+    // montei o corpo da mensagem
     var msgBody = {
-        email: txtEmail,
+        email : txtEmail,
         senha : txtSenha
     }
-       // como é solicitacao POST, temos que montar um objeto de cabeçalho
-    // como se fizessemos a seleção la no POSTMAN, indicnado que o método é
-    // POST, o corpo é o objeto msgBody convertido para String e o cabeçalho é
-    // par aindicar que essa STRING vai no formato JSON)
+
+
+    // como é solicitacao POST, temos que montar um objeto de cabeçalho 
+    // (como se fizessemos a seleção la no POSTMAN, indicando que o método é 
+    //  POST, o corpo é o objeto msgBody convertido para STRING e o cabeçalho é
+    //  pra indicar que essa STRING vai no formato JSON) 
     var cabecalho = {
-        method : "POST",
+        method : 'POST',
         body   : JSON.stringify(msgBody),
         headers : {
-                'Content-Type' : 'application/json'
+            'Content-Type': 'application/json'
         }
     }
-fetch("http://localhost:8080/login", cabecalho)
-    .then(res => res.json())
-    .then(res => logar(res))
-    .catch(err => trataErro(err));
+
+    fetch("http://localhost:8080/login",cabecalho)
+        .then(res => res.json())
+        .then(res => logar(res))
+        .catch(err => trataErro(err))
 }
 
 function logar(res){
-    //alert("login bem sucedido!!!");
-    console.log (res);
+    //console.log(res);
     localStorage.setItem("VMuser", JSON.stringify(res));
     window.location = "perfil.html";
 }
 
 function trataErro(err){
-        console.log(err);
-        document.getElementById("msg").style="visibility:visible";
+    console.log(err);
+    document.getElementById("msg").style="visibility:visible";
 }
