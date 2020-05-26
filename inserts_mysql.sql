b6865401302aff40667c7ed8b3d1d3113cbddb66
olara3 <felipe.pinga@gmail.com>
2020-05-26 18:38:52 -0300

    Add files via upload
    para criar o banco de dados

---
+++ inserts_mysql.sql
@@ -0,0 +1,17 @@
+create database vmperuser;
+use vmperuser;
+insert into tbl_usuario values (null,'adriano@adriano.com.br','link','Adriano','ADRIANO','1234','ABCD','(11)123451234');
+insert into tbl_usuario values (null,'guilherme@guilherme.com.br','link','Guilherme','GUILHER','1234','ABCD','(11)123451234');
+insert into tbl_usuario values (null,'luiz@luiz.com.br','link','Luiz','LUIZ123','1234','ABCD','(11)123451234');
+insert into tbl_usuario values (null,'julio@julio.com.br','link','Julio','JULIO12','1234','ABCD','(11)123451234');
+ 
+insert into tbl_software values (null, 'Microsoft', 'SQL Server', 100);
+insert into tbl_software values (null, 'Microsoft', 'Windows', 500);
+insert into tbl_software values (null, 'RedHat', 'Linux', 300);
+insert into tbl_software values (null, 'Oracle', 'MySql', 50);
+
+insert into tbl_solicitacao values (null,'2020/04/21','Primeira Solicitacao','1');
+insert into tbl_solicitacao values (null,'2020/03/21','Segunda Solicitacao','1');
+insert into tbl_solicitacao values (null,'2020/05/21','Terceira Solicitacao','1');
+insert into tbl_solicitacao values (null,'2020/04/25','Terceira Solicitacao','2');
+insert into tbl_solicitacao values (null,'2020/03/25','Terceira Solicitacao','2');
